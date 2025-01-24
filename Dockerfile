6f1637fc6946a873b2bb1a14b647d8ecf4df64e1
Daniel Suchan <mr.sucik@gmail.com>
2025-01-24 15:15:21 +0100

    feat: add template fly.io config and remove unrelated stuff

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,6 @@ RUN tar zxvf /tmp/Cronicle-${CRONICLE_VERSION}.tar.gz -C /tmp/ && \
     mv /tmp/Cronicle-${CRONICLE_VERSION}/* . && \
     rm -rf /tmp/* && \
     yarn
-COPY ./patches /tmp/patches
-RUN patch -p3 < /tmp/patches/engine.patch lib/engine.js
 COPY docker-entrypoint.js ./bin/
 
 
@@ -19,7 +17,8 @@ WORKDIR /opt/cronicle
 ENV CRONICLE_foreground=1
 ENV CRONICLE_echo=1
 ENV CRONICLE_color=1
-ENV debug_level=1
+ENV debug_level=9
 ENV HOSTNAME=main
+ENV CRONICLE_Storage__Filesystem__base_dir=/data/cronicle
 RUN node bin/build.js dist && bin/control.sh setup
 CMD ["node", "bin/docker-entrypoint.js"]
--- docker-entrypoint.js
+++ docker-entrypoint.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env node
 
-if(require("fs").existsSync("./data/users") || process.env["IS_WORKER"] === "true") {
+if(require("node:fs").existsSync("/data/cronicle/users") || process.env["IS_WORKER"] === "true") {
   console.log("Docker Env already configured.");
   require("../lib/main.js");
 } else {
-  const { existsSync, unlinkSync } = require("fs");
-  const { dirname } = require("path");
-  const { spawnSync } = require("child_process");
-  const { hostname, networkInterfaces } = require("os");
+  const { existsSync, unlinkSync, mkdirSync } = require("node:fs");
+  const { dirname } = require("node:path");
+  const { spawnSync } = require("node:child_process");
+  const { hostname, networkInterfaces } = require("node:os");
   const StandaloneStorage = require("pixl-server-storage/standalone");
 
   if (existsSync("./logs/cronicled.pid")) unlinkSync("./logs/cronicled.pid");
 
-  if (!existsSync("./data/users")) {
+  // Ensure the storage directory exists
+  if (!existsSync("/data/cronicle")) {
+    mkdirSync("/data/cronicle", { recursive: true });
+  }
+
+  if (!existsSync("/data/cronicle/users")) {
     console.log("Storage init.");
     const result = spawnSync("/opt/cronicle/bin/control.sh", ["setup"]);
     if (result.error || result.stderr.length !== 0) {
-      console.log("init strorage failed");
+      console.log("init storage failed");
       console.log(result.error?.message || result.stderr.toString());
       process.exit(1);
     }
@@ -26,7 +31,7 @@ if(require("fs").existsSync("./data/users") || process.env["IS_WORKER"] === "tru
   process.chdir(dirname(__dirname));
 
   const config = require("../conf/config.json");
-  const storage = new StandaloneStorage(config.Storage, function (err) {
+  const storage = new StandaloneStorage(config.Storage, (err) => {
     if (err) throw err;
     const dockerHostName = (process.env["HOSTNAME"] || process.env["HOST"] || hostname()).toLowerCase();
 
@@ -41,16 +46,16 @@ if(require("fs").existsSync("./data/users") || process.env["IS_WORKER"] === "tru
     };
 
     const key = "global/servers/0";
-    storage.put(key, data, function () {
-      storage.shutdown(function () {
-        console.log("Record successfully saved: " + key + "\n");
-        storage.get(key, function (_, data) {
+    storage.put(key, data, () => {
+      storage.shutdown(() => {
+        console.log(`Record successfully saved: ${key}\n`);
+        storage.get(key, (_, data) => {
           if (storage.isBinaryKey(key)) {
-            console.log(data.toString() + "\n");
+            console.log(`${data.toString()}\n`);
           } else {
-            console.log((typeof data == "object" ? JSON.stringify(data, null, "\t") : data) + "\n");
+            console.log(`${typeof data === "object" ? JSON.stringify(data, null, "\t") : data}\n`);
           }
-          storage.shutdown(function () {
+          storage.shutdown(() => {
             console.log("Docker Env Fixed.");
             require("../lib/main.js");
           });
